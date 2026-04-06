cc2a09e762c171e48502ed1edc5c95ca5f84d834
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2026-04-06 17:43:22 +0700

    mengisi data

--- structNew.cpp
+++ structNew.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat : ";
+    cin >> mhs.alamat;
+
+    cout << "Umur : ";
+    cin >> mhs.umur;
+
+    return 0;
+}
